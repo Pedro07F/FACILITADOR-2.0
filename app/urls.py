97e623eeba8b722838e6d5bd2fe46a3c6eb8c67b
Pedro07F <pedrohff2606@gmail.com>
2025-03-06 18:58:53 -0300

    Alteração urls

--- app/urls.py
+++ app/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from principal.views import principal_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('principal/',)
+    path('principal/',principal_view),
 ]
