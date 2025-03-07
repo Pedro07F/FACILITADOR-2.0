c67e4110a6bc6cc0e0b4887a13764ea4871e888a
Pedro07F <pedrohff2606@gmail.com>
2025-03-06 21:23:54 -0300

    Super User

--- principal/admin.py
+++ principal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from principal.models import cadastro
 
-# Register your models here.
+class AdminPrincipal(admin.ModelAdmin):
+    list_display = ('nome', 'cnpj') #lista de campos na tabela
+    search_fields = ('nome', 'cnpj') #campo de busca
+
+admin.site.register(cadastro, AdminPrincipal) #função que liga a tabela ao admin
--- principal/views.py
+++ principal/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from principal.models import cadastro
+from django.http import HttpResponse
 
 def principal_view(request):
-    empresas = cadastro.objects.all()
-    return render(request,'cadastro.html',{'cadastro':cadastro})
-
+    '''empresas = cadastro.objects.all()
+    return render(request,'cadastro.html',{'cadastro':cadastro})'''
+    return HttpResponse('Meus Carros')
